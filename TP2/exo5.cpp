af145e678ae75949cae07c5fcc773fe7a620509a
Lucieguerin431 <lucie.guerin@edu.univ-eiffel.fr>
2023-03-22 21:36:39 +0100

    exo5

--- TP2/exo5.cpp
+++ TP2/exo5.cpp
@@ -8,21 +8,51 @@ void merge(Array& first, Array& second, Array& result);
 
 void splitAndMerge(Array& origin)
 {
-	// stop statement = condition + return (return stop the function even if it does not return anything)
+    // stop statement = condition + return (return stop the function even if it does not return anything)
+    if(origin.size()<=1){
+        return;
+    }
 
-	// initialisation
-	Array& first = w->newArray(origin.size()/2);
-	Array& second = w->newArray(origin.size()-first.size());
-	
-	// split
+    // initialisation
+    Array& first = w->newArray(origin.size()/2);
+    Array& second = w->newArray(origin.size()-first.size());
 
-	// recursiv splitAndMerge of lowerArray and greaterArray
+    // split
+    for(size_t i=0; i<first.size(); i++){
+        first[i]=origin[i];
+    }
+    for(size_t j=0; j<second.size(); j++){
+        second[j]=origin[first.size()+j];
+    }
 
-	// merge
+    // recursiv
+    splitAndMerge(first);
+    splitAndMerge(second);
+
+    // merge
+    merge(first,second,origin);
 }
 
 void merge(Array& first, Array& second, Array& result)
 {
+	int id=0;
+	int ig=0;
+
+	while(ig!=first.size() || id!=second.size()){
+		if(ig==first.size()){
+			result[ig+id]=second[id];
+			id++;
+		} else if(id==second.size()){
+			result[ig+id]=first[ig];	
+			ig++;	
+		} else if(first[ig]<second[id]){
+			result[ig+id]=first[ig];
+			ig++;
+		} else {
+			result[ig+id]=second[id];
+			id++;
+		}
+	}
 
 }
 
