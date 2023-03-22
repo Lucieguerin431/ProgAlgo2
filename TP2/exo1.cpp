9cf2a529ee4eba5abe51e315da73f031b754c20a
Lucieguerin431 <lucie.guerin@edu.univ-eiffel.fr>
2023-03-22 21:30:43 +0100

    exo1

--- TP2/exo1.cpp
+++ TP2/exo1.cpp
@@ -1,18 +1,23 @@
 #include <QApplication>
 #include <time.h>
-
 #include "tp2.h"
 
 MainWindow* w = nullptr;
 
 void selectionSort(Array& toSort){
-	// selectionSort
+    for (uint i=0; i<toSort.size(); i++)
+    {
+        for (uint j=0; j<toSort.size(); j++)
+        {
+            if (toSort[j] > toSort[i])
+                toSort.swap(i, j);
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-    uint elementCount=15; // number of elements to sort
     MainWindow::instruction_duration = 100; // delay between each array access (set, get, insert, ...)
     w = new TestMainWindow(selectionSort); // window which display the behavior of the sort algorithm
     w->show();
