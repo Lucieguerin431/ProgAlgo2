5e72bc28c428db67cedcac1e9940b202ee5ee421
Lucieguerin431 <lucie.guerin@edu.univ-eiffel.fr>
2023-03-22 21:34:11 +0100

    exo3

--- TP2/exo3.cpp
+++ TP2/exo3.cpp
@@ -5,11 +5,15 @@
 
 MainWindow* w=nullptr;
 
-
 void bubbleSort(Array& toSort){
-	// bubbleSort
+    for(size_t i=0; i<toSort.size()-1;i++){
+        for(size_t j=0; j<toSort.size()-i-1; j++){
+            if(toSort[j]>toSort[j+1]){
+                toSort.swap(j,j+1);
+            }
+        }
+    }
 }
-
 
 int main(int argc, char *argv[])
 {
